92cbd4baafbf60c22f301fe6da08395bfcc09f80
Unknown <briankirotich@gmail.com>
2018-04-08 17:04:51 -0400

    sstf  C implementation - initial commit

--- sstf.c
+++ sstf.c
@@ -3,52 +3,56 @@
 */
 
 #include <stdio.h>
-//#include <conio.h>
 #include <stdlib.h>
 #include <math.h>
 
 int main() {
-  int queue[100], t[100], head, seek=0, i, j, n,temp;
+  int queue[100], queue2[100], q_size, head, seek=0, temp;
   float avg;
 
-  printf("%s\n", "Enter the size of the queue");
-  scanf("%d", &n);
+  printf("%s\n", "-----SSTF Disk Scheduling Algorithm-----");
 
-  for(i=0; i<n; i++){
-      scanf("%d",&queue[i]);
+  printf("%s\n", "Enter the size of the queue");
+  scanf("%d", &q_size);
+
+  printf("%s\n", "Enter queue elements");
+  for(int i=0; i<=q_size; i++){
+    scanf("%d",&queue[i]);
   }
 
   printf("%s\n","Enter initial head position");
   scanf("%d", &head);
 
-  for(i=1; i<n; i++){
-    t[i] = abs(head-queue[i]);
+  //get distance from head of elems in queue
+  for(i=1; i<q_size; i++){
+    queue2[i] = abs(head-queue[i]);
   }
-  for(i=0; i<n; i++){
 
-    for(j=i+1; j<n;j++){
+  //swap elems based on their distance from each other
+  for(int i=0; i<q_size; i++){
+      for(int j=i+1; j<q_size;j++){
 
-        if(t[i]>t[j]){
-          temp = t[i];
-          t[i]=t[j];
-          t[j]=temp;
-          temp=queue[i];
-          queue[i]=queue[j];
-          queue[j]=temp;
+        if(queue2[i]>queue2[j]){
+            temp = queue2[i];
+            queue2[i]=queue[j];
+            queue2[j]=temp;
 
+            temp=queue[i];
+            queue[i]=queue[j];
+            queue[j]=temp;
         }
     }
 
   }
 
-  for(i=1; i<n-1; i++){
+  for(int i=1; i<q_size-1; i++){
     seek = seek+abs(head-queue[i]);
     head = queue[i];
   }
 
-  printf("\nTotal Seek Time os %d\t", seek);
-  avg = seek/(float)n;
-  printf("\nAverage Seek Time is %f\t", avg);
-  //getch();
+  printf("\nTotal seek time is %d\t",seek);
+  avg = seek/(float)q_size;
+  printf("\nAverage seek time is %f\t", avg);
+  
   return 0;
 }
