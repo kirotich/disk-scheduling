6f0e446611f3536e98237a33d172e4df66db8af1
Unknown <briankirotich@gmail.com>
2018-04-09 15:13:49 -0400

    a3 - initial commit, skeleton code

---
+++ disk_scheduling_algorithms.c
@@ -0,0 +1,169 @@
+#include <stdio.h>  //for printf and scanf
+#include <stdlib.h> //for malloc
+
+#define LOW 0
+#define HIGH 199
+#define START 53
+
+//compare function for qsort
+//you might have to sort the request array
+//use the qsort function 
+// an argument to qsort function is a function that compares 2 quantities
+//use this there.
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+//function to swap 2 integers
+void swap(int *a, int *b)
+{
+    if (*a != *b)
+    {
+        *a = (*a ^ *b);
+        *b = (*a ^ *b);
+        *a = (*a ^ *b);
+        return;
+    }
+}
+
+//Prints the sequence and the performance metric
+void printSeqNPerformance(int *request, int numRequest)
+{
+    int i, last, acc = 0;
+    last = START;
+    printf("\n");
+    printf("%d", START);
+    for (i = 0; i < numRequest; i++)
+    {
+        printf(" -> %d", request[i]);
+        acc += abs(last - request[i]);
+        last = request[i];
+    }
+    printf("\nPerformance : %d\n", acc);
+    return;
+}
+
+//access the disk location in FCFS
+void accessFCFS(int *request, int numRequest)
+{
+    //simplest part of assignment
+    printf("\n----------------\n");
+    printf("FCFS :");
+    printSeqNPerformance(request, numRequest);
+    printf("----------------\n");
+    return;
+}
+
+//access the disk location in SSTF
+void accessSSTF(int *request, int numRequest)
+{
+    //write your logic here
+    printf("\n----------------\n");
+    printf("SSTF :");
+    printSeqNPerformance(request, numRequest);
+    printf("----------------\n");
+    return;
+}
+
+//access the disk location in SCAN
+void accessSCAN(int *request, int numRequest)
+{
+    
+	//write your logic here
+    printf("\n----------------\n");
+    printf("SCAN :");
+    printSeqNPerformance(newRequest, newCnt);
+    printf("----------------\n");
+    return;
+}
+
+//access the disk location in CSCAN
+void accessCSCAN(int *request, int numRequest)
+{
+    //write your logic here
+    printf("\n----------------\n");
+    printf("CSCAN :");
+    printSeqNPerformance(newRequest, newCnt);
+    printf("----------------\n");
+    return;
+}
+
+//access the disk location in LOOK
+void accessLOOK(int *request, int numRequest)
+{
+    //write your logic here
+    printf("\n----------------\n");
+    printf("LOOK :");
+    printSeqNPerformance(newRequest, newCnt);
+    printf("----------------\n");
+    return;
+}
+
+//access the disk location in CLOOK
+void accessCLOOK(int *request, int numRequest)
+{
+    //write your logic here
+    printf("\n----------------\n");
+    printf("CLOOK :");
+    printSeqNPerformance(newRequest,newCnt);
+    printf("----------------\n");
+    return;
+}
+
+int main()
+{
+    int *request, numRequest, i,ans;
+
+    //allocate memory to store requests
+    printf("Enter the number of disk access requests : ");
+    scanf("%d", &numRequest);
+    request = malloc(numRequest * sizeof(int));
+
+    printf("Enter the requests ranging between %d and %d\n", LOW, HIGH);
+    for (i = 0; i < numRequest; i++)
+    {
+        scanf("%d", &request[i]);
+    }
+
+    printf("\nSelect the policy : \n");
+    printf("----------------\n");
+    printf("1\t FCFS\n");
+    printf("2\t SSTF\n");
+    printf("3\t SCAN\n");
+    printf("4\t CSCAN\n");
+    printf("5\t LOOK\n");
+    printf("6\t CLOOK\n");
+    printf("----------------\n");
+    scanf("%d",&ans);
+
+    switch (ans)
+    {
+    //access the disk location in FCFS
+    case 1: accessFCFS(request, numRequest);
+        break;
+
+    //access the disk location in SSTF
+    case 2: accessSSTF(request, numRequest);
+        break;
+
+        //access the disk location in SCAN
+     case 3: accessSCAN(request, numRequest);
+        break;
+
+        //access the disk location in CSCAN
+    case 4: accessCSCAN(request,numRequest);
+        break;
+
+    //access the disk location in LOOK
+    case 5: accessLOOK(request,numRequest);
+        break;
+
+    //access the disk location in CLOOK
+    case 6: accessCLOOK(request,numRequest);
+        break;
+
+    default:
+        break;
+    }
+    return 0;
+}
