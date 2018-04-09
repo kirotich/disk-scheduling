f38e3a383f8578049583efbf91391d24079a0f41
Unknown <briankirotich@gmail.com>
2018-04-08 23:11:22 -0400

    CSCAN initial commit

---
+++ cscan.c
@@ -0,0 +1,81 @@
+/*
+  C-SCAN: Circular Scan
+   - Scans towards the nearest end and works it way all the way to the end of the system.
+   - Once it hits the bottom or top it jumps to the other end and moves in the same direction.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+  int queue[20], q_size, head, i,j, seek=0, diff, max, temp, queue1[20], queue2[20], temp1=0, temp2=0;
+  float avg;
+
+  printf("%s\t", "Enter max range of disk");
+  scanf("%d", &max);
+
+  printf("%s\t", "Input no of disk locations");
+  scanf("%d", &q_size);
+
+  printf("%s\t", "Enter initial head position");
+  scanf("%d", &head);
+
+  printf("%s\n","Enter disk positions to be read");
+  for(i=1; i<=q_size; i++) {
+
+    scanf("%d", &temp);
+      if(temp >= head){
+          queue1[temp1] = temp;
+          temp1++;
+      } else {
+          queue2[temp2] = temp;
+          temp2++;
+      }
+  }
+  //sort both arrays
+  for(i=0; i<temp1-1; i++){
+    for(j=i+1; j<temp1; j++){
+        if(queue1[i] > queue[j]){
+          temp = queue1[i];
+          queue1[i] = queue1[j];
+          queue1[j] = temp;
+      }
+    }
+  }
+
+  for(i=0; i<temp2-1; i++){
+    for(j=i+1; j<temp2; j++){
+      if(queue2[i]>queue[j]){
+          temp = queue2[i];
+          queue2[i] = queue2[j];
+          queue2[j] = temp;
+      }
+    }
+  }
+
+  for(i=1,j=0; j<temp1; i++,j++){
+      queue[i] = queue1[j];
+  }
+
+  queue[i] = max;
+  queue[i+1] = 0;
+
+  for(i=temp1+3, j=0; j<temp2; i++, j++){
+      queue[i] = queue2[j];
+  }
+
+  queue[0] = head;
+
+  for(j=0; j<=q_size+1; j++){
+      diff=abs(queue[j+1] - queue[j]);
+        seek += diff;
+        printf("Disk head moves from %d to %d with seek %d\n",queue[j],queue[j+1],diff);
+
+  }
+
+  printf("Total seek time is %d\n", seek);
+  avg = seek/(float)q_size;
+  printf("Average seek time is %f\n", avg);
+
+  return 0;
+}
